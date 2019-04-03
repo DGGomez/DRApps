fee891f736e3f97f8095d5f221a8e68825eaf35b
Daniel Gomez <dggomez21@gmail.com>
2019-04-03 08:59:35 -0400

    Update basic.js

--- backend/routes/basic.js
+++ backend/routes/basic.js
@@ -4,22 +4,10 @@ var authenticate = require('../middleware/authenticate');
 module.exports = function(app, db) {
 var dataList = require('../controllers/dataController');
 
-app.route('/create')
-    .post(dataList.create);
-
 app.route('/read')
     .post(dataList.read);
     
-app.route('/archive')
-    .post(dataList.archive);
-
-app.post('/register')
-    .post(dataList.registerUser);
-
-app.post('/login')
-    .post(dataList.loginUser);
-
-app.post('/token-login')
-    .post(authenticate, dataList.getUser);
+app.route('/send')
+    .post(dataList.send);
 
 }
