910148acb23107cc473135ef1000ab09cd9a498b
Daniel Gomez <dggomez21@gmail.com>
2019-04-03 09:07:26 -0400

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -35,41 +35,5 @@ const db = mongoose.connection
     
 dataRoutes(app);
 
-var auth = {
-    type: 'oauth2',
-    user: 'WillMurrayApps@gmail.com',
-    clientId: process.env.ID,
-    clientSecret: process.env.SECRET,
-    refreshToken: process.env.REFRESH,
-};
-
-app.post('/send', function(req, res){
- var response = {
-    name: req.body.name,
-    email: req.body.email,
-    message: req.body.message
-  }
-  
-  
-  var mailOptions = {
-      from: req.body.name,
-      to: 'WillMurrayApps@gmail.com',
-      subject: 'My site contact from: ' + req.body.name,
-      text: req.body.message,
-      html: 'Message from: ' + req.body.name + '<br></br> Email: ' +  req.body.email + '<br></br> Message: ' + req.body.message,
-  };
-var transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: auth,
-  });
-transporter.sendMail(mailOptions, (err, res) => {
-      if (err) {
-          return console.log(err);
-      } else {
-          console.log(JSON.stringify(res));
-      }
-  });
-});
-
 //connection.end();
 module.exports = app;
