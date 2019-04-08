29a9d698aaad88044820681da3015374403f18b6
Daniel Gomez <dggomez21@gmail.com>
2019-04-08 10:51:06 -0400

    Update ProductPage.js

--- web/src/pages/ProductPage.js
+++ web/src/pages/ProductPage.js
@@ -1,16 +1,25 @@
 import React, {Component} from "react";
 import { connect } from 'react-redux';
-
+import readData from '../actions';
 import {Link} from "react-router-dom";
+import Card from '../componenets/Card';
 
 class AboutPage extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      var itemList;
     };
   }
 
+  componentDidMount(){
+  const {readData, data} = this.props;
+    readData({})
+    .then(res => this.setState({itemList: res.body}))
+    .catch(err => console.log(err));
+    
+  }
 
   render() {
     return (
@@ -21,7 +30,14 @@ class AboutPage extends Component {
         If you want to buy an app or give us a suggestion go to the contact page and leave us a message. Check
         out our other projects while you're here.
         </p></div>
-        
+        <div>
+      {this.state.itemList.map(() => {
+        return (
+          <Card title ={event} location = {links} description= {description}/> 
+        );
+        })}
+      
+        </div>
       </div>
 
     );
