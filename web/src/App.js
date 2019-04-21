115f171852e0464b27f8fe4785ab6ce6ec96cfd4
DGGomez <dggomez21@gmail.com>
2019-04-20 21:16:59 -0400

    added logo to page

--- web/src/App.js
+++ web/src/App.js
@@ -24,6 +24,7 @@ class App extends Component {
         <div className="container">
           <Router>
             <div style={{ marginTop: '7rem' }}>
+                <img src="./DownRampAppsLogo.png"/>
                 <NavBar />
                 <Route path="/" exact component={DashboardPage} />
                 <Route path="/dashboard" exact component={DashboardPage} />
